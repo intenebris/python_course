3e14cb8b7a77b71b5e5ed041f18be1922f1bd0d4
Viaintenebris <m.manivchuk@gmail.com>
2024-07-24 02:44:36 +0300

    Added lecture folder

---
+++ lecture_1/main.py
@@ -0,0 +1,15 @@
+# print('Введите первое число:')
+# a = input()
+# b = input('Введите второе число: ')
+# print(a, ' + ', b, ' + ', a+b)
+
+# c = 5.89
+# print(c)
+# print(type(c))
+# c = str(c)
+# print(c)
+# print(type(c))
+
+a = 5.757657
+b = 6.32131
+print(round(a*b, 3))
